2a6fcc4af64005d0d0875e53dc5d7796c092bb1f
seg <segnities007@gmail.com>
2024-12-31 14:48:27 +0900

    change search to trend, and modify floating action button, etc...

--- app/src/main/java/com/segnities007/seg/ui/screens/hub/Hub.kt
+++ app/src/main/java/com/segnities007/seg/ui/screens/hub/Hub.kt
@@ -1,18 +1,9 @@
 package com.segnities007.seg.ui.screens.hub
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -26,8 +17,8 @@ import com.segnities007.seg.ui.components.navigation_drawer.NavigationDrawer
 import com.segnities007.seg.ui.screens.hub.home.Home
 import com.segnities007.seg.ui.screens.hub.notify.Notify
 import com.segnities007.seg.ui.screens.hub.post.Post
-import com.segnities007.seg.ui.screens.hub.search.Search
 import com.segnities007.seg.ui.screens.hub.setting.Setting
+import com.segnities007.seg.ui.screens.hub.trend.Trend
 import com.segnities007.seg.ui.screens.login.NavigateAction
 import com.segnities007.seg.ui.screens.login.NavigateState
 
@@ -74,18 +65,12 @@ private fun HubUi(
             ) { navigateAction.onIndexChange(it) }
         },
         floatingActionButton = {
-            when(navigateState.index){
-                0 -> FloatingButton(iconID = R.drawable.baseline_home_24) { }
-                1 -> FloatingButton(iconID = R.drawable.baseline_search_24) { }
-                2 -> FloatingButton(iconID = R.drawable.baseline_post_add_24) { }
-                3 -> FloatingButton(iconID = R.drawable.baseline_notifications_24) { }
-                4 -> FloatingButton(iconID = R.drawable.baseline_settings_24) { }
-            }
+            FloatingButton(iconID = R.drawable.baseline_search_24) { }
         }
     ){ innerPadding ->
         when(navigateState.index){
             0 -> Home(modifier = Modifier.padding(innerPadding))
-            1 -> Search(modifier = Modifier.padding(innerPadding))
+            1 -> Trend(modifier = Modifier.padding(innerPadding))
             2 -> Post(modifier = Modifier.padding(innerPadding))
             3 -> Notify(modifier = Modifier.padding(innerPadding))
             4 -> Setting(modifier = Modifier.padding(innerPadding))
--- app/src/main/java/com/segnities007/seg/ui/screens/hub/home/PostCard.kt
+++ app/src/main/java/com/segnities007/seg/ui/screens/hub/home/PostCard.kt
@@ -1,15 +1,16 @@
 package com.segnities007.seg.ui.screens.hub.home
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,8 +22,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.segnities007.seg.R
 
@@ -134,13 +133,16 @@ private fun ActionIcon(
 ){
     Row(
         modifier = modifier
-            .clickable {  },
+            .clip(RoundedCornerShape(dimensionResource(R.dimen.padding_small)))
+            .clickable {  }
+            .padding(dimensionResource(R.dimen.padding_action_icon)),
     ){
         Image(
             painter = painterResource(painterRes),
             contentDescription = stringResource(contentRes),
             colorFilter = tint(MaterialTheme.colorScheme.secondary)
         )
+        Spacer(modifier = Modifier.padding(dimensionResource(R.dimen.padding_action_icon)))
         Text("0")
     }
 }
