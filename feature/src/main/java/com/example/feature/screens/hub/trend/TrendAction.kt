53c5e9f8638f416fbdc384e6e2133bebe487e8fa
seg <segnities007@gmail.com>
2025-06-07 22:39:48 +0900

    modify TrendAction and TrendState

--- feature/src/main/java/com/example/feature/screens/hub/trend/TrendAction.kt
+++ feature/src/main/java/com/example/feature/screens/hub/trend/TrendAction.kt
@@ -4,36 +4,36 @@ import androidx.compose.runtime.Immutable
 import com.example.domain.model.post.Post
 
 @Immutable
-sealed class TrendAction {
-    data object GetAdditionalTrendPostOfToday : TrendAction()
+sealed interface TrendAction {
+    data object GetAdditionalTrendPostOfToday : TrendAction
 
-    data object GetAdditionalTrendPostOfWeek : TrendAction()
+    data object GetAdditionalTrendPostOfWeek : TrendAction
 
-    data object GetAdditionalTrendPostOfMonth : TrendAction()
+    data object GetAdditionalTrendPostOfMonth : TrendAction
 
-    data object GetAdditionalTrendPostOfYear : TrendAction()
+    data object GetAdditionalTrendPostOfYear : TrendAction
 
     data class GetTrendPostOfToday(
         val limit: Long,
-    ) : TrendAction()
+    ) : TrendAction
 
     data class GetTrendPostOfWeek(
         val limit: Long,
-    ) : TrendAction()
+    ) : TrendAction
 
     data class GetTrendPostOfMonth(
         val limit: Long,
-    ) : TrendAction()
+    ) : TrendAction
 
     data class GetTrendPostOfYear(
         val limit: Long,
-    ) : TrendAction()
+    ) : TrendAction
 
     data class ProcessOfEngagement(
         val newPost: Post,
-    ) : TrendAction()
+    ) : TrendAction
 
-    data object Init : TrendAction()
+    data object Init : TrendAction
 
-    data object Dispose : TrendAction()
+    data object Dispose : TrendAction
 }
