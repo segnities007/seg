82c0478110fdc7b4e223b3a9d3c6bac3f1a092ed
seg <segnities007@gmail.com>
2025-06-07 22:33:08 +0900

    modify TopLayerState

--- feature/src/main/java/com/example/feature/navigation/TopLayerState.kt
+++ feature/src/main/java/com/example/feature/navigation/TopLayerState.kt
@@ -2,7 +2,9 @@ package com.example.feature.navigation
 
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import com.example.feature.model.UiStatus
 
 data class TopLayerState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val drawerState: DrawerState = DrawerState(initialValue = DrawerValue.Closed),
 )
