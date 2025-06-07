90f9707416ec9c3861179e7e7262f1acbee9b941
seg <segnities007@gmail.com>
2025-06-07 22:41:19 +0900

    modify CreateAccountState

--- feature/src/main/java/com/example/feature/screens/login/sign_up/create_account/CreateAccountState.kt
+++ feature/src/main/java/com/example/feature/screens/login/sign_up/create_account/CreateAccountState.kt
@@ -1,9 +1,11 @@
 package com.example.feature.screens.login.sign_up.create_account
 
 import android.net.Uri
+import com.example.feature.model.UiStatus
 import java.time.LocalDate
 
 data class CreateAccountState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val isShow: Boolean = false,
     val name: String = "",
     val userID: String = "",
