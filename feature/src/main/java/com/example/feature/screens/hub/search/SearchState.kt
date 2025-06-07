19df30a7d01488dd242fb05778f584ea1f439234
seg <segnities007@gmail.com>
2025-06-07 22:37:08 +0900

    modify SearchState

--- feature/src/main/java/com/example/feature/screens/hub/search/SearchState.kt
+++ feature/src/main/java/com/example/feature/screens/hub/search/SearchState.kt
@@ -2,8 +2,10 @@ package com.example.feature.screens.hub.search
 
 import com.example.domain.model.post.Post
 import com.example.domain.model.user.User
+import com.example.feature.model.UiStatus
 
 data class SearchState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val users: List<User> = listOf(),
     val posts: List<Post> = listOf(),
     val postsSortedByViewCount: List<Post> = listOf(),
