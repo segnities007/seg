a6d7503386bd5525b90ac0756457645b80c8c9db
seg <segnities007@gmail.com>
2025-06-07 22:37:36 +0900

    modify MyPostsState

--- feature/src/main/java/com/example/feature/screens/hub/setting/my_posts/MyPostsState.kt
+++ feature/src/main/java/com/example/feature/screens/hub/setting/my_posts/MyPostsState.kt
@@ -3,9 +3,11 @@ package com.example.feature.screens.hub.setting.my_posts
 import androidx.compose.runtime.Immutable
 import com.example.domain.model.post.Post
 import com.example.domain.model.user.User
+import com.example.feature.model.UiStatus
 
 @Immutable
 data class MyPostsState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val self: User = User(),
     val posts: List<Post> = listOf(),
     val likedPosts: List<Post> = listOf(),
