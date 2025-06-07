9612df27885c0cfeb99a8329520537386c8f9ce2
seg <segnities007@gmail.com>
2025-06-07 22:38:26 +0900

    modify SettingState

--- feature/src/main/java/com/example/feature/screens/hub/setting/SettingState.kt
+++ feature/src/main/java/com/example/feature/screens/hub/setting/SettingState.kt
@@ -1,6 +1,9 @@
 package com.example.feature.screens.hub.setting
 
+import com.example.feature.model.UiStatus
+
 data class SettingState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val isDatePickerDialogShow: Boolean = false,
     val newName: String = "",
     val newUserID: String = "",
