46a10207691e658773ef50ce4a0b04f30a4fe5cb
seg <segnities007@gmail.com>
2025-06-07 22:36:32 +0900

    modify PostState

--- feature/src/main/java/com/example/feature/screens/hub/post/PostState.kt
+++ feature/src/main/java/com/example/feature/screens/hub/post/PostState.kt
@@ -1,8 +1,10 @@
 package com.example.feature.screens.hub.post
 
 import com.example.domain.model.post.Genre
+import com.example.feature.model.UiStatus
 
 data class PostState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val inputText: String = "",
     val isLoading: Boolean = false,
     val genre: Genre = Genre.NORMAL,
