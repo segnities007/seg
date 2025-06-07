a81569ed0d8f082da05b6809e43b1c8bc4fe54fa
seg <segnities007@gmail.com>
2025-06-08 02:00:31 +0900

    modify HomeViewModel

--- feature/src/main/java/com/example/feature/screens/hub/home/HomeViewModel.kt
+++ feature/src/main/java/com/example/feature/screens/hub/home/HomeViewModel.kt
@@ -28,38 +28,41 @@ class HomeViewModel
                 is HomeAction.ChangeIsAllPostsFetched,
                 -> homeState = homeReducer(action, homeState)
 
-                is HomeAction.GetNewPosts -> {
-                    viewModelScope.launch(Dispatchers.IO) {
-                        val posts = postRepository.onGetNewPosts(action.genre)
-                        homeState =
-                            when (action.genre) {
-                                Genre.HAIKU -> homeState.copy(haikus = posts)
-                                Genre.TANKA -> homeState.copy(tankas = posts)
-                                Genre.KATAUTA -> homeState.copy(katautas = posts)
-                                Genre.SEDOUKA -> homeState.copy(sedoukas = posts)
-                                else -> homeState.copy(posts = posts)
-                            }
-                    }
-                }
+                is HomeAction.GetNewPosts -> getNewPosts(action)
+                is HomeAction.GetBeforeNewPosts -> getBeforeNewPosts(action)
+            }
+        }
 
-                is HomeAction.GetBeforeNewPosts -> {
-                    viewModelScope.launch(Dispatchers.IO) {
-                        val posts =
-                            postRepository.onGetBeforePosts(
-                                afterPostCreateAt = action.updatedAt,
-                                genre = action.genre,
-                            )
-                        if (posts.isEmpty()) homeReducer(action, homeState)
-                        homeState =
-                            when (action.genre) {
-                                Genre.HAIKU -> homeState.copy(haikus = homeState.haikus.plus(posts))
-                                Genre.TANKA -> homeState.copy(tankas = homeState.tankas.plus(posts))
-                                Genre.KATAUTA -> homeState.copy(katautas = homeState.katautas.plus(posts))
-                                Genre.SEDOUKA -> homeState.copy(sedoukas = homeState.sedoukas.plus(posts))
-                                else -> homeState.copy(posts = homeState.posts.plus(posts))
-                            }
+        private fun getNewPosts(action: HomeAction.GetNewPosts) {
+            viewModelScope.launch(Dispatchers.IO) {
+                val posts = postRepository.onGetNewPosts(action.genre)
+                homeState =
+                    when (action.genre) {
+                        Genre.HAIKU -> homeState.copy(haikus = posts)
+                        Genre.TANKA -> homeState.copy(tankas = posts)
+                        Genre.KATAUTA -> homeState.copy(katautas = posts)
+                        Genre.SEDOUKA -> homeState.copy(sedoukas = posts)
+                        else -> homeState.copy(posts = posts)
                     }
-                }
+            }
+        }
+
+        private fun getBeforeNewPosts(action: HomeAction.GetBeforeNewPosts) {
+            viewModelScope.launch(Dispatchers.IO) {
+                val posts =
+                    postRepository.onGetBeforePosts(
+                        afterPostCreateAt = action.updatedAt,
+                        genre = action.genre,
+                    )
+                if (posts.isEmpty()) homeReducer(action, homeState)
+                homeState =
+                    when (action.genre) {
+                        Genre.HAIKU -> homeState.copy(haikus = homeState.haikus.plus(posts))
+                        Genre.TANKA -> homeState.copy(tankas = homeState.tankas.plus(posts))
+                        Genre.KATAUTA -> homeState.copy(katautas = homeState.katautas.plus(posts))
+                        Genre.SEDOUKA -> homeState.copy(sedoukas = homeState.sedoukas.plus(posts))
+                        else -> homeState.copy(posts = homeState.posts.plus(posts))
+                    }
             }
         }
     }
