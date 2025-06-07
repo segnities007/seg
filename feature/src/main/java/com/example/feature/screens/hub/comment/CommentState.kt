a03aeb0ad9d37c0568e2ec27d48f02e15973f946
seg <segnities007@gmail.com>
2025-06-07 22:35:09 +0900

    modify CommentState

--- feature/src/main/java/com/example/feature/screens/hub/comment/CommentState.kt
+++ feature/src/main/java/com/example/feature/screens/hub/comment/CommentState.kt
@@ -2,8 +2,10 @@ package com.example.feature.screens.hub.comment
 
 import androidx.compose.runtime.Immutable
 import com.example.domain.model.post.Post
+import com.example.feature.model.UiStatus
 
 @Immutable
 data class CommentState(
+    val uiStatus: UiStatus = UiStatus.Initial,
     val comments: List<Post> = listOf(),
 )
