e7ca49f0f1dc5b04f17f8b08d9c779588c8b56ea
seg <segnities007@gmail.com>
2025-06-08 11:46:01 +0900

    modify TabState

--- feature/src/main/java/com/example/feature/components/tab/TabState.kt
+++ feature/src/main/java/com/example/feature/components/tab/TabState.kt
@@ -1,9 +1,6 @@
 package com.example.feature.components.tab
 
-import com.example.feature.model.UiStatus
-
 data class TabState(
-    val uiStatus: UiStatus = UiStatus.Initial,
     val index: Int = 0,
     val labels: List<String> = listOf(),
 )
