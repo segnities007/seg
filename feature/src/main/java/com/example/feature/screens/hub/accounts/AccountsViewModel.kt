15b953a45acb6beb5f7cc5d1ec27a814d42d42a5
seg <segnities007@gmail.com>
2025-06-09 08:25:04 +0900

    deal with Error of Accounts method

--- feature/src/main/java/com/example/feature/screens/hub/accounts/AccountsViewModel.kt
+++ feature/src/main/java/com/example/feature/screens/hub/accounts/AccountsViewModel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.repository.UserRepository
+import com.example.feature.model.UiStatus
+import com.example.feature.screens.hub.HubAction
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -30,16 +32,36 @@ class AccountsViewModel
         }
 
         private fun getUser(action: AccountsAction.GetUser) {
+            accountsState = accountsState.copy(uiStatus = UiStatus.Loading)
             viewModelScope.launch(Dispatchers.IO) {
-                val user = userRepository.onGetOtherUser(action.userID)
-                accountsState = accountsState.copy(user = user)
+                try {
+                    val user = userRepository.onGetOtherUser(action.userID)
+                    accountsState = accountsState.copy(user = user)
+                    accountsState = accountsState.copy(uiStatus = UiStatus.Success)
+                } catch (e: Exception) {
+                    accountsState =
+                        accountsState.copy(uiStatus = UiStatus.Error("ユーザーの取得に失敗しました。"))
+                    action.onHubAction(HubAction.OpenSnackBar((accountsState.uiStatus as UiStatus.Error).message))
+                } finally {
+                    accountsState = accountsState.copy(uiStatus = UiStatus.Initial)
+                }
             }
         }
 
         private fun getUsers(action: AccountsAction.GetUsers) {
+            accountsState = accountsState.copy(uiStatus = UiStatus.Loading)
             viewModelScope.launch(Dispatchers.IO) {
-                val users = userRepository.onGetUsers(action.userIDs)
-                accountsState = accountsState.copy(users = users)
+                try {
+                    val users = userRepository.onGetUsers(action.userIDs)
+                    accountsState = accountsState.copy(users = users)
+                    accountsState = accountsState.copy(uiStatus = UiStatus.Success)
+                } catch (e: Exception) {
+                    accountsState =
+                        accountsState.copy(uiStatus = UiStatus.Error("ユーザーの取得に失敗しました。"))
+                    action.onHubAction(HubAction.OpenSnackBar((accountsState.uiStatus as UiStatus.Error).message))
+                } finally {
+                    accountsState = accountsState.copy(uiStatus = UiStatus.Initial)
+                }
             }
         }
     }
